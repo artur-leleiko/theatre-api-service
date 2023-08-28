c058acc4f3661e4e5e9fd955c18983cc4fb037fc
Artur Leleiko <artur_98@ukr.net>
2023-08-28 22:40:06 +0300

    Create models: TheatreHall, Genre, Actor

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        ordering = ["name"]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        ordering = ["name"]
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=125, required=True)
+    last_name = models.CharField(max_length=125, required=True)
+    
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+    
+    class Meta:
+        ordering = ["first_name"]
