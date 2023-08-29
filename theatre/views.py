43118ba63d30b2e42a7d2e1db0fd85326f060ed7
Artur Leleiko <artur_98@ukr.net>
2023-08-29 22:31:49 +0300

    Create views for all models

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import TheatreHall, Genre, Actor, Play, Performance
+from theatre.serializers import (
+    TheatreHallSerializer,
+    GenreSerializer,
+    ActorSerializer,
+    PlaySerializer,
+    PerformanceSerializer
+)
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.select_related("play", "theatre_hall")
+    serializer_class = PerformanceSerializer
