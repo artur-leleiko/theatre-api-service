4af6f78ba91a88dedf072d5cd5f9541034a413c6
Artur Leleiko <artur_98@ukr.net>
2023-09-03 19:10:25 +0300

    Add filtering by title, genres and actors for Plays

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from rest_framework import viewsets
 
 from theatre.models import (
@@ -50,6 +51,32 @@ class PlayViewSet(viewsets.ModelViewSet):
             return PlayDetailSerializer
 
         return PlaySerializer
+
+    @staticmethod
+    def _params_to_ints(qs: list[str]) -> list[int]:
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self) -> QuerySet:
+        """Retrieve the plays with filters"""
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._params_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
 
 
 class PerformanceViewSet(viewsets.ModelViewSet):
